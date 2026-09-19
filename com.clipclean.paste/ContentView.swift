ef6e3d29907d87ab78bed4cd0d696fe4c64023a9
henryliu443 <henryliu443@gmail.com>
2026-09-20 03:10:47 +0800

    Initial Commit

---
+++ com.clipclean.paste/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  com.clipclean.paste
+//
+//  Created by 刘博文 on 20/9/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ com.clipclean.paste/com_clipclean_pasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  com_clipclean_pasteApp.swift
+//  com.clipclean.paste
+//
+//  Created by 刘博文 on 20/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct com_clipclean_pasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
